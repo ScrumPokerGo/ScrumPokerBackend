cc96dafcc1eb3db6d121ddc3184fd9ded145b2f0
pepe <jose.torres@lambdamotive.com>
2018-02-16 21:10:44 +0100

    Basic structure

--- src/ScrumPokerBackend/main.go
+++ src/ScrumPokerBackend/main.go
@@ -4,10 +4,10 @@ import (
 	"net/http"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/gorilla/mux"
-	"log"
+	"github.com/gorilla/handlers"
 	"ScrumPokerBackend/database"
 	"ScrumPokerBackend/routers"
-
+	"log"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -15,7 +15,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
 
 	database.Init()
 	defer database.Close()
@@ -29,47 +28,13 @@ func main() {
 	//user := model.GetUser(1)
 	//fmt.Println(user.Name)
 
+	router := mux.NewRouter()
+	routers.AddAccountRouters(router)
 
-	 router := mux.NewRouter()
-	 routers.AddAccountRouters(router)
 
-
-	 log.Fatal(http.ListenAndServe(":8000", router))
-/*
-	type StatusError struct {
-		Status int
-		// Allows you to wrap another error
-		Err error
-	}
-
-	func (e *StatusError) Error() string {
-		return e.Error()
-	}
-
-	type AppHandler func(w http.ResponseWriter, r *http.Request) error
 
 	// Satisfies the http.Handler interface
-	func (ah AppHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Centralises your error handling
-	err := ah(w, r)
-	if err != nil {
-	switch e := a.(type) {
-	case *StatusError:
-	switch e.Status {
-	case 400:
-	http.Error(w, e.Err.Error(), 400)
-	return
-	case 404:
-	http.NotFound(w, r)
-	return
-	default:
-	http.Error(w, http.StatusText(500), 500)
-	return
-	}
-	default:
-	http.Error(w, http.StatusText(500), 500)
-	return
-	}
-	}
-*/
-	}
+
+
+	log.Fatal(http.ListenAndServe(":8000", handlers.RecoveryHandler()(router)))
+}
--- src/ScrumPokerBackend/routers/account.go
+++ src/ScrumPokerBackend/routers/account.go
@@ -7,6 +7,7 @@ import(
 )
 
 func AddAccountRouters(r *mux.Router){
+
 	r.HandleFunc("/account", handlers.GetAccounts).Methods("GET")
 	r.HandleFunc("/account/{id}", handlers.GetAccount).Methods("GET")
 	r.HandleFunc("/account/", handlers.CreateAccount).Methods("POST")
